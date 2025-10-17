993765c93f72c04908b90199003b70cfa9fa695c
Bima Adnandita <bima.adnandita@gmail.com>
2025-10-17 15:22:46 +0700

    add post to genres and authors

--- app/Http/Controllers/AuthorController.php
+++ app/Http/Controllers/AuthorController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Author;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class AuthorController extends Controller
 {
@@ -23,5 +24,35 @@ class AuthorController extends Controller
             "message" => "Get all resource",
             "data" => $authors,
         ], 200 );
+    }
+
+    public function store(Request $request){
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:100',
+            'bio' => 'required|string',
+            'photo' => 'required|image|mimes:jpeg,jpg,png|max:2048',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'message' => $validator->errors(),
+            ], 422);
+        }
+
+        $image = $request->file('photo');
+        $image->store('authors', 'public');
+
+        $author = Author::create([
+            'name' => $request->name,
+            'bio' => $request->bio,
+            'photo' => $image->hashName(),
+        ]);
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Author created successfully',
+            'data' => $author,
+        ], 201);
     }
 }
--- app/Http/Controllers/GenreController.php
+++ app/Http/Controllers/GenreController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Genre;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class GenreController extends Controller
 {
@@ -23,5 +24,30 @@ class GenreController extends Controller
             "message" => "Get all resource",
             "data" => $genres,
         ], 200);
+    }
+
+    public function store(Request $request){
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:100',
+            'description' => 'required|string',
+        ]);
+
+        if ($validator->fails()){
+            return response()->json([
+                'success' => false,
+                'message' => $validator->errors(),
+            ], 422);
+        }
+
+        $genre = Genre::create([
+            'name' => $request->name,
+            'description' => $request->description,
+        ]);
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Genre created successfully',
+            'data' => $genre,
+        ], 201);
     }
 }
